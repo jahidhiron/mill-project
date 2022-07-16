b61a1b4141baa43b39d0098daaaa9ff56f8acbab
Jahidul Islam Hiron <namehiron.96@gmail.com>
2022-07-16 14:20:10 +0600

    db module

--- backend/src/api/v1/product/Model.js
+++ backend/src/api/v1/product/Model.js
@@ -4,11 +4,6 @@ const { Schema, model } = require('mongoose');
 const productSchema = Schema({
   name: { type: String, trim: true, required: true },
   desc: { type: String },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'admin',
-    required: true,
-  },
 });
 
 module.exports = model('product', productSchema);
